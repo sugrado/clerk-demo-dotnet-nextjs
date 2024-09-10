2fcc7b95fcf25dbe7029641a3e397124af867b03
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2024-09-10 22:01:50 +0300

    refactor: remove unused config initializator

--- server/Extensions/ConfigHelper.cs
+++ server/Extensions/ConfigHelper.cs
@@ -2,12 +2,6 @@
 
 public static class ConfigHelper
 {
-    public static IConfiguration? Config;
-    public static void Initialize(IConfiguration configuration)
-    {
-        Config = configuration;
-    }
-
     public static T GetOptions<T>(this IConfiguration configuration, string sectionName) where T : new()
         => configuration.GetSection(sectionName).Get<T>()
             ?? throw new InvalidOperationException($"{sectionName} section cannot found in configuration.");
